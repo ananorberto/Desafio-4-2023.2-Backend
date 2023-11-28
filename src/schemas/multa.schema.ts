de996debfa6392f708846c97b6bd4edaf442e11b
ananorberto <norbertoana16@gmail.com>
2023-11-27 23:07:11 -0300

    add schemas

---
+++ src/schemas/multa.schema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const MultaSchema = z.object({
+    ID: z.number().int(),
+    valor: z.number().positive(),
+    data: z.date(),
+    pontosPenalidade: z.number().int().positive(),
+    tipoInfração: z.string().max(200),
+});
+
+
+export type Multa = z.infer<typeof MultaSchema>;
+
+
