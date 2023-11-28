911cb6d4242fd58004f8f0fda2136422bf719f4c
ananorberto <norbertoana16@gmail.com>
2023-11-27 23:12:57 -0300

    update routing

--- src/routing.ts
+++ src/routing.ts
@@ -1,5 +1,11 @@
 import { type Express } from "express";
+import motoristaRouter from "./routes/motorista.route";
+import veiculoRouter from "./routes/veiculo.route";
+import multaRouter from "./routes/multa.route";
 
 export default function routing(app: Express) {
   // ! Include your routes here !
+  app.use("/api/motorista", motoristaRouter);
+  app.use("/api/veiculo", veiculoRouter);
+  app.use("/api/multa", multaRouter);
 }
